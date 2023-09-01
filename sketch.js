4a68346432379e4cf193798009cf4b1ee4979b90
taiteb <taiteb@gmail.com>
2023-08-31 23:22:37 -0600

    add case for diagonal movement, adl. step variance

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let c;
 
 function setup() {
   c = createCanvas(700, 700);
-  background(51);
-  for (let i = 0; i < 150; i++){
+  // background(51);
+  for (let i = 0; i < 150; i++) {
     let newWalker = new Walker();
     walkers.push(newWalker);
   }
@@ -18,8 +18,8 @@ function draw() {
 }
 
 function Walker() {
-  let x = random(100, 600);
-  let y = random(100, 600);
+  let x = random(200, 500);
+  let y = random(200, 500);
   // console.log(x, y);
 
   let r = floor(random(120));
@@ -27,22 +27,39 @@ function Walker() {
   let b = floor(random(120, 255));
 
   let rstroke = random(5);
+  let rstep = floor(random(5))
 
   this.update = function () {
-    let r = floor(random(4));
+    let r = floor(random(8));
     // console.log(r)
     switch (r) {
       case 0:
-        x = x + 2;
+        x = x + rstep;
         break;
       case 1:
-        x = x - 2;
+        x = x - rstep;
         break;
       case 2:
-        y = y + 2;
+        y = y + rstep;
         break;
       case 3:
-        y = y - 2;
+        y = y - rstep;
+        break;
+      case 4:
+        x = x + rstep;
+        y = y + rstep;
+        break;
+      case 5:
+        x = x - rstep;
+        y = y + rstep;
+        break;
+      case 6:
+        x = x - rstep;
+        y = y - rstep;
+        break;
+      case 7:
+        x = x + rstep;
+        y = y - rstep;
         break;
     }
     stroke(r, g, b);
@@ -51,6 +68,6 @@ function Walker() {
   }
 }
 
-function mouseClicked(){
-  save(c, `${frameCount}.jpg`)
+function mouseClicked() {
+  save(c, `${frameCount}.png`)
 }
