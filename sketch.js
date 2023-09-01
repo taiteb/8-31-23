45ce7a8a975b904efcac03900ec8c702413030ee
taiteb <taiteb@gmail.com>
2023-08-31 21:22:51 -0600

    add: rand. stroke, more walkers

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let walkers = [];
 function setup() {
   createCanvas(700, 700);
   background(51);
-  for (let i = 0; i < 16; i++){
+  for (let i = 0; i < 150; i++){
     let newWalker = new Walker();
     walkers.push(newWalker);
   }
@@ -25,6 +25,8 @@ function Walker() {
   let g = floor(random(255));
   let b = floor(random(120, 255));
 
+  let rstroke = random(5);
+
   this.update = function(){
     let r = floor(random(4));
     // console.log(r)
@@ -43,7 +45,7 @@ function Walker() {
         break;
     }
     stroke(r, g, b);
-    strokeWeight(2);
+    strokeWeight(rstroke);
     point(x, y);
   }
 }
